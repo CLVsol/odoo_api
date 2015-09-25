9f4ee5280392207e63e7b45e8f37870328210463
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2015-09-25 13:30:50 -0300

    Updated "clv_medicament_list.py".

--- clv_medicament_list.py
+++ clv_medicament_list.py
@@ -28,6 +28,53 @@ from base import *
 import argparse
 import getpass
 
+def clv_medicament_list_updt_medicament_orizon(client, list_name, list_version_name):
+
+    clv_medicament_list = client.model('clv_medicament_list')
+    medicament_list_browse = clv_medicament_list.browse([('name', '=', list_name),])
+    print('>>>>>', medicament_list_browse)
+
+    clv_medicament_list_version = client.model('clv_medicament_list.version')
+    medicament_list_version_browse = clv_medicament_list_version.browse(
+        [('list_id', '=', medicament_list_browse[0].id),
+         ('name', '=', list_version_name),
+         ])
+    print('>>>>>', medicament_list_version_browse)
+
+    clv_medicament_list_item = client.model('clv_medicament_list.item')
+    medicament_list_item_browse = clv_medicament_list_item.browse(
+        [('list_version_id', '=', medicament_list_version_browse[0].id),
+         ('medicament_id', '=', False),
+         ])
+
+    i = 0
+    found = 0
+    not_found = 0
+    for medicament_list_item in medicament_list_item_browse:
+        i += 1
+        print(i, medicament_list_item)
+
+        if medicament_list_item.medicament_ref != False:
+            clv_medicament = client.model('clv_medicament')
+            medicament_browse = clv_medicament.browse(\
+                [('orizon_lpm_id', '=', medicament_list_item.medicament_ref.id),])
+            print('>>>>>', medicament_browse)
+
+            if medicament_browse.id != []:
+                found += 1
+                values = {
+                    'medicament_id': medicament_browse[0].id,
+                    }
+                clv_medicament_list_item.write(medicament_list_item.id, values)
+            else:
+                not_found += 1
+        else:
+            not_found += 1
+
+    print('--> i: ', i)
+    print('--> found: ', found)
+    print('--> not_found: ', not_found)
+
 def get_arguments():
 
     global username
@@ -75,6 +122,14 @@ if __name__ == '__main__':
     start = time()
 
     print('--> clv_medicament_list.py...')
+
+    client = erppeek.Client(server, dbname, username, password)
+
+    # list_name = 'Orizon 483 (0,5k)'
+    # list_version_name = '1508'
+    # print('-->', client, list_name, list_version_name)
+    # print('--> Executing clv_medicament_list_updt_medicament_orizon()...')
+    # clv_medicament_list_updt_medicament_orizon(client, list_name, list_version_name)
 
     print('--> clv_medicament_list.py')
     print('--> Execution time:', secondsToStr(time() - start))
