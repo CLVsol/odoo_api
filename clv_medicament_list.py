322c49ba73d9025bfa61abf39dac616fb8086ddd
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2015-09-25 13:35:18 -0300

    Updated "clv_medicament_list.py".

--- clv_medicament_list.py
+++ clv_medicament_list.py
@@ -106,6 +106,75 @@ def clv_medicament_list_clear_subsidy_orizon(client, list_name, list_version_nam
 
     print('--> i: ', i)
 
+def clv_medicament_list_check_orizon(client, infile_name, list_name, list_version_name):
+
+    delimiter_char = ';'
+
+    f  = open(infile_name, "rb")
+    r = csv.reader(f, delimiter=delimiter_char)
+    rownum = 0
+    found = 0
+    not_found = 0
+    for row in r:
+
+        if rownum == 0:
+            rownum += 1
+            continue
+
+        i = autoIncrement(0, 1)
+
+        cod_prod = row[i.next()]
+        medicament = row[i.next()]
+        discount = row[i.next()]
+        preco_venda = row[i.next()].replace(",", ".")
+        Subsidio = row[i.next()]
+
+        print(rownum, cod_prod, medicament)
+
+        clv_medicament_list = client.model('clv_medicament_list')
+        medicament_list_browse = clv_medicament_list.browse([('name', '=', list_name),])
+        print('>>>>>', medicament_list_browse)
+
+        clv_medicament_list_version = client.model('clv_medicament_list.version')
+        medicament_list_version_browse = clv_medicament_list_version.browse(
+            [('list_id', '=', medicament_list_browse[0].id),
+             ('name', '=', list_version_name),
+             ])
+        print('>>>>>', medicament_list_version_browse)
+
+        clv_orizon_lpm = client.model('clv_orizon_lpm')
+        orizon_lpm_browse = clv_orizon_lpm.browse([('cod_prod', '=', cod_prod),])
+        print('>>>>>', orizon_lpm_browse)
+
+        clv_medicament = client.model('clv_medicament')
+        medicament_browse = clv_medicament.browse([('orizon_lpm_id', '=', orizon_lpm_browse[0].id),])
+        print('>>>>>', medicament_browse)
+
+        clv_medicament_list_item = client.model('clv_medicament_list.item')
+        medicament_list_item_browse = clv_medicament_list_item.browse(
+            [('list_version_id', '=', medicament_list_version_browse[0].id),
+             ('medicament_id', '=', medicament_browse[0].id),
+             ])
+        print('>>>>>', medicament_list_item_browse, medicament_list_item_browse.medicament_ref)
+        medicament_list_item_id = medicament_list_item_browse.id
+
+        if medicament_list_item_id != []:
+            found += 1
+            values = {
+                'subsidy': 100.0,
+                }
+            clv_medicament_list_item.write(medicament_list_item_id[0], values)
+        else:
+            not_found += 1
+
+        rownum += 1
+
+    f.close()
+
+    print('--> rownum: ', rownum - 1)
+    print('--> found: ', found)
+    print('--> not_found: ', not_found)
+
 def get_arguments():
 
     global username
@@ -167,6 +236,13 @@ if __name__ == '__main__':
     # print('-->', client, list_name, list_version_name)
     # print('--> Executing clv_medicament_list_clear_subsidy_orizon()...')
     # clv_medicament_list_clear_subsidy_orizon(client, list_name, list_version_name)
+
+    # infile_name = '/opt/openerp/orizon_lpm/Lista_483_0_5k_LPM_1508.csv'
+    # list_name = 'Orizon 483 (0,5k)'
+    # list_version_name = '1508'
+    # print('-->', client, infile_name, list_name, list_version_name)
+    # print('--> Executing clv_medicament_list_check_orizon()...')
+    # clv_medicament_list_check_orizon(client, infile_name, list_name, list_version_name)
 
     print('--> clv_medicament_list.py')
     print('--> Execution time:', secondsToStr(time() - start))
