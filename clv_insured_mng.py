f114db8960baf09fa7498721abeb961a346ae410
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2015-10-13 11:16:49 -0300

    Updated "clv_insured_mng.py".

--- clv_insured_mng.py
+++ clv_insured_mng.py
@@ -1197,12 +1197,17 @@ if __name__ == '__main__':
     # print('--> Executing clv_batch_producao_export_protocolo()...')
     # clv_batch_producao_export_protocolo(client, file_path, PRODUCTION_BATCH_NAME)
 
-    batch_args = [('state', '=', 'checking'), 
-                  ('name_category', '=', 'Grupo Familiar'),
-                  ]
-    print('-->', client, batch_args)
-    print('--> Executing clv_batch_updt_state_done()...')
-    clv_batch_updt_state_done(client, batch_args)
+    # batch_args = [('state', '=', 'checking'), 
+    #               ('name_category', '=', 'Grupo Familiar'),
+    #               ]
+    # print('-->', client, batch_args)
+    # print('--> Executing clv_batch_updt_state_done()...')
+    # clv_batch_updt_state_done(client, batch_args)
+
+    # batch_args = [('state', '=', 'checking'),]
+    # print('-->', client, batch_args)
+    # print('--> Executing clv_batch_updt_state_done()...')
+    # clv_batch_updt_state_done(client, batch_args)
 
     print('--> clv_insured_mng.py')
     print('--> Execution time:', secondsToStr(time() - start))
