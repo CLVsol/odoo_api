23a807280381af4cf2743d71b0a18d150f2d9fca
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2015-10-29 16:09:42 -0200

    Updated "clv_medicament_dispensation.py".

--- clv_medicament_dispensation.py
+++ clv_medicament_dispensation.py
@@ -385,5 +385,16 @@ if __name__ == '__main__':
     # print('--> Executing clv_medicament_dispensation_import_dispensation_ext_orizon()...')
     # clv_medicament_dispensation_import_dispensation_ext_orizon(client)
 
+    # print('-->', client)
+    # print('--> Executing clv_medicament_dispensation_updt_mrp()...')
+    # clv_medicament_dispensation_updt_mrp(client)
+    
+    # file_path = "/opt/openerp/biobox/data/bb_dispensation_2015_09_21_a_10_20.csv"
+    # start_date = '2015-09-21'
+    # end_date = '2015-10-20'
+    # print('-->', client, file_path, start_date, end_date)
+    # print('--> Executing clv_medicament_dispensation_export()...')
+    # clv_medicament_dispensation_export(client, file_path, start_date, end_date)
+
     print('--> clv_medicament_dispensation.py')
     print('--> Execution time:', secondsToStr(time() - start))
