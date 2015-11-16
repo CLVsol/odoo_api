a3daeb534cbd6e61c8defa8560bd046deeb9e154
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2015-11-16 13:33:41 -0200

    Updated "clv_insured_ext.py".

--- clv_insured_ext.py
+++ clv_insured_ext.py
@@ -696,6 +696,12 @@ if __name__ == '__main__':
     # print('--> Executing clv_insured_ext_sync_confirm_orizon() for "' + file_name + '"...')
     # clv_insured_ext_sync_confirm_orizon(client, file_name, date_synchronization)
 
+    #######################################
+
+    # print('-->', client)
+    # print('--> Executing clv_insured_ext_updt_from_insured_card_orizon()...')
+    # clv_insured_ext_updt_from_insured_card_orizon(client)
+
     # print('-->', client)
     # file_name = '/opt/openerp/orizon/USU1865_20151116_151102_I.TXT'
     # print('--> Executing clv_insured_ext_syncronize_orizon(' + file_name + ')...')
