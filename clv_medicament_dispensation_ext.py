65432e49866e2286ea4c27d29764b55c69fddd8f
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2016-02-05 09:19:40 -0200

    Updated "clv_medicament_dispensation_ext.py".

--- clv_medicament_dispensation_ext.py
+++ clv_medicament_dispensation_ext.py
@@ -764,5 +764,40 @@ if __name__ == '__main__':
     # print('--> Executing clv_medicament_dispensation_ext_updt_dispensation()...')
     # clv_medicament_dispensation_ext_updt_dispensation(client)
 
+    #######################################
+
+    # file_name = '/opt/openerp/orizon/Desconto_em_Folha-Analitico_21-01-2016_ate_31-01-2016.csv'
+    # print('-->', client, file_name)
+    # print('--> Executing clv_medicament_dispensation_ext_import_orizon()...')
+    # clv_medicament_dispensation_ext_import_orizon(client, file_name)
+
+    # print('-->', client)
+    # print('--> Executing clv_medicament_dispensation_ext_updt_name()...')
+    # clv_medicament_dispensation_ext_updt_name(client)
+
+    # print('-->', client)
+    # print('--> Executing clv_medicament_dispensation_ext_updt_pharmacy()...')
+    # clv_medicament_dispensation_ext_updt_pharmacy(client)
+
+    # print('-->', client)
+    # print('--> Executing clv_medicament_dispensation_ext_updt_prescriber()...')
+    # clv_medicament_dispensation_ext_updt_prescriber(client)
+
+    # print('-->', client)
+    # print('--> Executing clv_medicament_dispensation_ext_updt_insured_card()...')
+    # clv_medicament_dispensation_ext_updt_insured_card(client)
+
+    # print('-->', client)
+    # print('--> Executing clv_medicament_dispensation_ext_updt_medicament_ref_orizon()...')
+    # clv_medicament_dispensation_ext_updt_medicament_ref_orizon(client)
+
+    # print('-->', client)
+    # print('--> Executing clv_medicament_dispensation_ext_updt_medicament()...')
+    # clv_medicament_dispensation_ext_updt_medicament(client)
+
+    # print('-->', client)
+    # print('--> Executing clv_medicament_dispensation_ext_updt_dispensation()...')
+    # clv_medicament_dispensation_ext_updt_dispensation(client)
+
     print('--> clv_medicament_dispensation_ext.py')
     print('--> Execution time:', secondsToStr(time() - start))
