ed0633d90e4129216bba884915a8c34a04cd91f5
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2016-02-25 08:13:33 -0300

    Updated file "clv_insured_ext.py".

--- clv_insured_ext.py
+++ clv_insured_ext.py
@@ -809,5 +809,22 @@ if __name__ == '__main__':
     # print('--> Executing clv_insured_ext_sync_confirm_orizon() for "' + file_name + '"...')
     # clv_insured_ext_sync_confirm_orizon(client, file_name, date_synchronization)
 
+    #######################################
+
+    # print('-->', client)
+    # print('--> Executing clv_insured_ext_updt_from_insured_card_orizon()...')
+    # clv_insured_ext_updt_from_insured_card_orizon(client)
+
+    # print('-->', client)
+    # file_name = '/opt/openerp/orizon/USU1865_20160225_160202_I.TXT'
+    # print('--> Executing clv_insured_ext_syncronize_orizon(' + file_name + ')...')
+    # clv_insured_ext_syncronize_orizon(client, file_name)
+
+    # print('-->', client)
+    # file_name = '/opt/openerp/orizon/USU1865_20160225_160202_I.TXT'
+    # date_synchronization = '2016-02-25 21:00:00'
+    # print('--> Executing clv_insured_ext_sync_confirm_orizon() for "' + file_name + '"...')
+    # clv_insured_ext_sync_confirm_orizon(client, file_name, date_synchronization)
+
     print('--> clv_insured_ext.py')
     print('--> Execution time:', secondsToStr(time() - start))
