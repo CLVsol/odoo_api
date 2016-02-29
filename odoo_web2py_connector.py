5738ab3d3c1f754a01f87a65f44d4158ebe23193
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2016-02-29 10:05:16 -0300

    Updated file "odoo_web2py_connector.py".

--- odoo_web2py_connector.py
+++ odoo_web2py_connector.py
@@ -20,25 +20,78 @@
 
 from __future__ import print_function
 
+import xmlrpclib
+import argparse
+import getpass
+
 
 def secondsToStr(t):
+
     return "%d:%02d:%02d.%03d" % reduce(lambda ll, b: divmod(ll[0], b) + ll[1:], [(t*1000,), 1000, 60, 60])
 
 
 def tester():
+
     import xmlrpclib
     server = xmlrpclib.ServerProxy('http://localhost:8000/odoo_web2py_connector/default/call/xmlrpc')
     return str(server.add(9, 3) + server.sub(2, 9))
 
 
+def get_arguments():
+
+    global username
+    global password
+    global dbname
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--user', action="store", dest="username")
+    parser.add_argument('--pw', action="store", dest="password")
+    parser.add_argument('--db', action="store", dest="dbname")
+
+    args = parser.parse_args()
+    print('%s%s' % ('--> ', args))
+
+    if args.dbname is not None:
+        dbname = args.dbname
+    elif dbname == '*':
+        dbname = raw_input('dbname: ')
+
+    if args.username is not None:
+        username = args.username
+    elif username == '*':
+        username = raw_input('username: ')
+
+    if args.password is not None:
+        password = args.password
+    elif password == '*':
+        password = getpass.getpass('password: ')
+
 if __name__ == '__main__':
+
+    server = 'http://localhost:8069'
+
+    username = 'username'
+    # username = '*'
+    paswword = 'paswword'
+    # paswword = '*'
+
+    dbname = 'odoo'
+    # dbname = '*'
+
+    get_arguments()
 
     from time import time
     start = time()
 
     print('--> odoo_web2py_connector.py...')
 
-    print(tester())
+    # print(tester())
+
+    server = xmlrpclib.ServerProxy('http://localhost:8000/odoo_web2py_connector/default/call/xmlrpc')
+    print(str(server.user_test(username,
+                               paswword,
+                               username,
+                               paswword)))
 
     print('--> odoo_web2py_connector.py')
     print('--> Execution time:', secondsToStr(time() - start))
