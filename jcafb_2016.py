f9c679e2296bbb684a17f6fe987b97bf619fd88b
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2016-03-11 09:37:06 -0300

    Added file "jcafb_2016.py".

---
+++ jcafb_2016.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+###############################################################################
+#                                                                             #
+# Copyright (C) 2016-Today  Carlos Eduardo Vercelino - CLVsol                 #
+#                                                                             #
+# This program is free software: you can redistribute it and/or modify        #
+# it under the terms of the GNU Affero General Public License as published by #
+# the Free Software Foundation, either version 3 of the License, or           #
+# (at your option) any later version.                                         #
+#                                                                             #
+# This program is distributed in the hope that it will be useful,             #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU Affero General Public License for more details.                         #
+#                                                                             #
+# You should have received a copy of the GNU Affero General Public License    #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
+###############################################################################
+
+from __future__ import print_function
+
+from erppeek import *
+
+from base import *
+import argparse
+import getpass
+
+from ir_model_data import *
+
+
+def get_arguments():
+
+    global username
+    global password
+    global dbname
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--user', action="store", dest="username")
+    parser.add_argument('--pw', action="store", dest="password")
+    parser.add_argument('--db', action="store", dest="dbname")
+
+    args = parser.parse_args()
+    print('%s%s' % ('--> ', args))
+
+    if args.dbname is not None:
+        dbname = args.dbname
+    elif dbname == '*':
+        dbname = raw_input('dbname: ')
+
+    if args.username is not None:
+        username = args.username
+    elif username == '*':
+        username = raw_input('username: ')
+
+    if args.password is not None:
+        password = args.password
+    elif password == '*':
+        password = getpass.getpass('password: ')
+
+
+if __name__ == '__main__':
+
+    server = 'http://localhost:8069'
+
+    # username = 'username'
+    username = '*'
+    # paswword = 'paswword'
+    paswword = '*'
+
+    dbname = 'odoo'
+    # dbname = '*'
+
+    get_arguments()
+
+    from time import time
+    start = time()
+
+    print('--> survey.py...')
+
+    client = erppeek.Client(server, dbname, username, password)
+
+    print('--> survey.py')
+    print('--> Execution time:', secondsToStr(time() - start))
