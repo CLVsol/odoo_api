7a749fc4949bad6b63705a50efcaaaa4db69b2ae
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2016-03-27 10:15:30 -0300

    Updated file "jcafb_2016.py".

--- jcafb_2016.py
+++ jcafb_2016.py
@@ -1499,6 +1499,12 @@ if __name__ == '__main__':
     # print('--> survey_question_user_input_line_values_sqlite()...')
     # survey_question_user_input_line_values_sqlite(client, db_path, code)
 
+    # db_path = '/opt/openerp/jcafb/data/jcafb_2016.sqlite'
+    # code = 'QMD16_03_08'
+    # print('-->', client, db_path, code)
+    # print('--> survey_question_user_input_line_values_sqlite()...')
+    # survey_question_user_input_line_values_sqlite(client, db_path, code)
+
     #####################################
 
     # file_path = '/opt/openerp/jcafb/data/jcafb_2016_02_QDH16_03_02_QDH16_06_03.csv'
@@ -1735,6 +1741,16 @@ if __name__ == '__main__':
     # file_path = '/opt/openerp/jcafb/data/jcafb_2016_34_ISE16_09_03_QDH16_08_02_QDH16_08_07_QDH16_08_12.csv'
     # db_path = '/opt/openerp/jcafb/data/jcafb_2016.sqlite'
     # code_1 = 'ISE16_09_03'
+    # code_2 = 'QDH16_08_02'
+    # code_3 = 'QDH16_08_07'
+    # code_4 = 'QDH16_08_12'
+    # print('-->', client, file_path, db_path, code_1, code_2, code_3, code_4)
+    # print('--> jcafb_2016_export_4()...')
+    # jcafb_2016_export_4(client, file_path, db_path, code_1, code_2, code_3, code_4)
+
+    # file_path = '/opt/openerp/jcafb/data/jcafb_2016_34_QMD16_03_08_QDH16_08_02_QDH16_08_07_QDH16_08_12.csv'
+    # db_path = '/opt/openerp/jcafb/data/jcafb_2016.sqlite'
+    # code_1 = 'QMD16_03_08'
     # code_2 = 'QDH16_08_02'
     # code_3 = 'QDH16_08_07'
     # code_4 = 'QDH16_08_12'
