a00d3e1ed943022496f99d65c9e7cc94a4f3be59
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-07-02 20:23:01 +0000

    [IMP] clv_medicament_dispensation.py

--- clv_medicament_dispensation.py
+++ clv_medicament_dispensation.py
@@ -1802,5 +1802,26 @@ if __name__ == '__main__':
     # print('--> Executing clv_medicament_dispensation_export()...')
     # clv_medicament_dispensation_export(client, file_path, start_date, end_date)
 
+    # ##### (2019-07-02) ######################################
+
+    # print('-->', client)
+    # print('--> Executing clv_medicament_dispensation_import_dispensation_ext_orizon()...')
+    # clv_medicament_dispensation_import_dispensation_ext_orizon(client)
+
+    # print('-->', client)
+    # print('--> Executing clv_medicament_dispensation_updt_mrp()...')
+    # clv_medicament_dispensation_updt_mrp(client)
+
+    # print('-->', client)
+    # print('--> Executing clv_medicament_dispensation_updt_refund_price()...')
+    # clv_medicament_dispensation_updt_refund_price(client)
+
+    # file_path = "/opt/openerp/biobox/data/bb_dispensation_2019_06_01_a_2019_06_30.csv"
+    # start_date = '2019-06-01'
+    # end_date = '2019-06-30'
+    # print('-->', client, file_path, start_date, end_date)
+    # print('--> Executing clv_medicament_dispensation_export()...')
+    # clv_medicament_dispensation_export(client, file_path, start_date, end_date)
+
     print('--> clv_medicament_dispensation.py')
     print('--> Execution time:', secondsToStr(time() - start))
